5dd96c7cedb665341e1b0e9f6ddf4e32bdd660a8
Daniel Kaschel <dipique@gmail.com>
2017-07-17 12:41:35 -0500

    Create first web view (not yet working)

---
+++ Data Editor/Controllers/HeroController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using DotA;
+using DotA.Model;
+using DotA.WebEdit.Models;
+
+namespace Data_Editor.Controllers
+{
+    public class HeroController : Controller
+    {
+        // GET: Hero
+        public ActionResult Index()
+        {
+            DotaData data = DotaData.Load();
+            return View("Heroes", new DynMultiView<Hero>(data.Heroes));
+        }
+
+        public ActionResult HeroView(string heroName)
+        {
+            DotaData data = DotaData.Load();
+            return View("Hero", new DynSingleView<Hero>(data.Heroes.FirstOrDefault(h => h.Name == heroName)));
+        }
+    }
+}
---
+++ Data Editor/Models/DynView.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace DotA.WebEdit.Models
+{
+    public class DynMultiView<T> where T : class
+    {
+        private Type srcType = null;
+        public List<DisplayValue> DisplayValues { get; private set; }
+        public List<T> Objects { get; set; }
+
+        public DynMultiView(List<T> objects)
+        {
+            srcType = typeof(T);
+            Objects = objects;
+            DisplayValues = srcType.GetProperties().Select(p => new DisplayValue(p)).ToList();
+        }
+    }
+
+    public class DynSingleView<T> where T : class
+    {
+        private Type srcType = null;
+        public List<DisplayValue> DisplayValues { get; private set; }
+        public T Object { get; set; }
+
+        public DynSingleView(T obj)
+        {
+            srcType = typeof(T);
+            Object = obj;
+            DisplayValues = srcType.GetProperties().Select(p => new DisplayValue(p)).ToList();
+        }
+    }
+
+    public class DisplayValue
+    {
+        private PropertyInfo srcProperty = null;
+        public string PropertyName => srcProperty?.Name;
+
+        public DisplayValue(PropertyInfo property)
+        {
+            srcProperty = property;
+        }
+
+        public DisplayValueType Type
+        {
+            get
+            {
+                if (srcProperty.PropertyType == typeof(string)) return DisplayValueType.String;
+                if (srcProperty.PropertyType == typeof(decimal)) return DisplayValueType.Decimal;
+                if (srcProperty.PropertyType.IsArray) return DisplayValueType.DecimalArray;
+                if (srcProperty.PropertyType.IsEnum) return DisplayValueType.PickList;
+                return DisplayValueType.Other;
+            }
+        }
+
+        public string[] GetPicklistOptions() => Type != DisplayValueType.PickList ? null
+                                                                                  : Enum.GetNames(srcProperty.PropertyType);
+        public string GetValue(object src)
+        {
+            object value = null;
+            //try {
+            value = srcProperty.GetValue(src);
+            //} catch { }
+
+            switch (Type)
+            {                
+                case DisplayValueType.DecimalArray: return string.Join(" ", (decimal[])value);
+                default: return value.ToString();    //TODO: Handle flags
+            }
+        }
+    }
+
+    public enum DisplayValueType
+    {
+        Other = 0,
+        Decimal,
+        DecimalArray,
+        String,
+        PickList //enum
+    }
+}
