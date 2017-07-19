baa363f05d257eef7e147a7a3a9bbb0fc5774607
Daniel Kaschel <dipique@gmail.com>
2017-07-19 09:19:36 -0500

    Added persistent data instead of reload every time Prevent lists from appearing on entry forms Made first working display form of properties Took out expression generation code because it was stupid

--- Data Editor/Controllers/HeroController.cs
+++ Data Editor/Controllers/HeroController.cs
@@ -12,19 +12,25 @@ namespace Data_Editor.Controllers
 {
     public class HeroController : Controller
     {
+        /// <summary>
+        /// Store the data in a persistent session
+        /// </summary>
         const string dataLocation = @"c:\users\dkaschel\desktop\dota.dat";
+        const string dataInd = "data";
+        public DotaData db => (DotaData)(Session[dataInd] == null ? (Session[dataInd] = DotaData.Load(dataLocation)) 
+                                                                  : Session[dataInd]);
 
         // GET: Hero
         public ActionResult Index()
         {
             DotaData data = DotaData.Load(dataLocation);
-            return View("Heroes", new DynMultiView<Hero>(data.Heroes));
+            return View("Heroes", new DynMultiView<Hero>(db.Heroes));
         }
 
         public ActionResult Hero(string heroName)
         {
             DotaData data = DotaData.Load(dataLocation);
-            return View("HeroView", new DynSingleView<Hero>(data.Heroes.FirstOrDefault(h => h.Name == heroName)));
+            return View("HeroView", new DynSingleView<Hero>(db.Heroes.FirstOrDefault(h => h.Name == heroName)));
         }
     }
 }
--- Data Editor/Models/DynView.cs
+++ Data Editor/Models/DynView.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Web;
+using System.Web.Mvc;
 
 namespace DotA.WebEdit.Models
 {
@@ -17,14 +17,15 @@ namespace DotA.WebEdit.Models
         }
     }
 
-    public class DynSingleView<T> : DynView<T> where T: class
-    {        
+    public class DynSingleView<T> : DynView<T> where T : class
+    {
         public T Item { get; set; }
         public DynSingleView(T obj)
         {
             srcType = typeof(T);
             Item = obj;
         }
+
     }
 
     /// <summary>
@@ -35,7 +36,7 @@ namespace DotA.WebEdit.Models
     public abstract class DynView<T> where T : class
     {
         protected Type srcType = null;
-
+        public string Title { get; set; }
         private List<DisplayValue> displayValues = null;
         public List<DisplayValue> DisplayValues
         {
@@ -43,6 +44,7 @@ namespace DotA.WebEdit.Models
             {
                 if (displayValues == null)
                     displayValues = srcType.GetProperties().Where(p => p.CanRead)
+                                                           .Where(p => !p.PropertyType.IsGenericType)
                                                            .Select(p => new DisplayValue(p)).ToList();
                 return displayValues;
             }
@@ -53,6 +55,7 @@ namespace DotA.WebEdit.Models
     {
         private PropertyInfo srcProperty = null;
         public string PropertyName => srcProperty?.Name;
+        public bool Editable { get; set; } = true;
 
         public DisplayValue(PropertyInfo property)
         {
@@ -66,13 +69,23 @@ namespace DotA.WebEdit.Models
                 if (srcProperty.PropertyType == typeof(string)) return DisplayValueType.String;
                 if (srcProperty.PropertyType == typeof(decimal)) return DisplayValueType.Decimal;
                 if (srcProperty.PropertyType.IsArray) return DisplayValueType.DecimalArray;
-                if (srcProperty.PropertyType.IsEnum) return DisplayValueType.PickList;
+                if (srcProperty.PropertyType.IsEnum) return srcProperty.GetCustomAttribute<FlagsAttribute>() == null ? DisplayValueType.PickList
+                                                                                                                     : DisplayValueType.PickList_Multi;
                 return DisplayValueType.Other;
             }
         }
 
         public string[] GetPicklistOptions() => Type != DisplayValueType.PickList ? null
                                                                                   : Enum.GetNames(srcProperty.PropertyType);
+        public List<SelectListItem> PicklistOptionsAsListItems(object src)
+        {
+            var selections = GetValue(src).Split(',').Select(s => s.Trim());
+            return GetPicklistOptions().Select(s => new SelectListItem() {
+                Selected = selections.Any(sel => sel == s),
+                Text = s
+            }).ToList();
+        }
+
         public string GetValue(object src)
         {
             object value = null;
@@ -82,8 +95,12 @@ namespace DotA.WebEdit.Models
 
             switch (Type)
             {                
-                case DisplayValueType.DecimalArray: return string.Join(" ", (decimal[])value);
-                default: return value?.ToString();    //TODO: Handle flags
+                case DisplayValueType.DecimalArray:
+                    return string.Join(" ", (decimal[])value);
+                case DisplayValueType.PickList_Multi:
+                    
+                default:
+                    return value?.ToString();    //TODO: Handle flags
             }
         }
     }
@@ -94,6 +111,7 @@ namespace DotA.WebEdit.Models
         Decimal,
         DecimalArray,
         String,
-        PickList //enum
+        PickList, //enum
+        PickList_Multi //flags
     }
 }
